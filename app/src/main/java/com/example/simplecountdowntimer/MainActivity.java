059fa567d42e3137813a2a12cdb3f9c95df9ee3d
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2019-10-30 00:11:43 -0700

    This is a working countdown timer hard coded to start at 10 minutes

--- app/src/main/java/com/example/simplecountdowntimer/MainActivity.java
+++ app/src/main/java/com/example/simplecountdowntimer/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.simplecountdowntimer;
 
+/**********************************************************************
+*                                                                     *
+*   From YouTube Video: https://www.youtube.com/watch?v=zmjfAcnosS0   *
+*                                                                     *
+***********************************************************************/
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView countDownText;
+    private Button countDownButton;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 600000; //600,000 = 10 minutes
+    private boolean timerRunning;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countDownText = findViewById(R.id.countdown_text);
+        countDownButton = findViewById(R.id.countdown_button);
+
+        countDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
+
+        updateTimer();
+    }
+
+    public void startStop () {
+        if (timerRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        countDownButton.setText("PAUSE");
+        timerRunning = true;
+    }
+
+    public void stopTimer() {
+        countDownTimer.cancel();
+        countDownButton.setText("START");
+        timerRunning = false;
+    }
+
+    public void updateTimer() {
+        int minutes = (int) timeLeftInMilliseconds / 60000;
+        int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if(seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        countDownText.setText((timeLeftText));
     }
 }
