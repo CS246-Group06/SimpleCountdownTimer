e1c684b3bc0ef44ade6b53b2a9178ccf31696cb9
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2019-11-09 21:49:10 -0800

    Label can be updated by user. Rest timer is working.

--- app/src/main/java/com/example/simplecountdowntimer/MainActivity.java
+++ app/src/main/java/com/example/simplecountdowntimer/MainActivity.java
@@ -10,26 +10,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_TIME = "com.example.simplecountdowntimer.TIME";
+    public static final String EXTRA_NAME = "com.example.simplecountdowntimer.NAME";
 
     private TextView countDownText;
+    private TextView labelName;
+    private TextView setRest;
     private Button countDownButton;
+    private Button setReset;
+    //private String timerLabel = "Timer 1";
+   // private TextView timerLabel2;
     private CountDownTimer countDownTimer;
     private long timeLeftInMilliseconds = 0; //600,000 = 10 minutes
+    private long resetTime;
     private boolean timerRunning;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        labelName = findViewById(R.id.labelName);
+        setReset = findViewById(R.id.setReset);
+        setReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTimer();
+            }
+        });
         countDownText = findViewById(R.id.countDownText);
         countDownButton = findViewById(R.id.countdown_button);
         countDownButton.setOnClickListener(new View.OnClickListener() {
@@ -38,16 +52,29 @@ public class MainActivity extends AppCompatActivity {
                 startStop();
             }
         });
+
+        // Sets the default name of the timer
+        labelName.setText(("Timer 1"));
+
+        // Updates variables with time and label set by user
         Intent intent =getIntent();
         timeLeftInMilliseconds = getIntent().getLongExtra(EXTRA_TIME, 0);
+        resetTime = timeLeftInMilliseconds;
+        labelName.setText(intent.getStringExtra(EXTRA_NAME));
+
+        // Updates the Text box displaying time set by user
         updateTimer();
     }
 
-    public void setTimer (View view) {
+
+    public void setTimer () {
+        // If there is time left on the timer, RESET it
         if(timeLeftInMilliseconds > 0) {
-            timeLeftInMilliseconds = 0;
+            timeLeftInMilliseconds = resetTime;
+            startStop();
             updateTimer();
         }
+        // If no time left on the timer, go to SetTimer activity
         else {
             Intent intent = new Intent(this, SetTimer.class);
             startActivity(intent);
@@ -78,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
         }.start();
 
         countDownButton.setText("PAUSE");
+        setReset.setText("RESET");
         timerRunning = true;
     }
 
--- app/src/main/java/com/example/simplecountdowntimer/SetTimer.java
+++ app/src/main/java/com/example/simplecountdowntimer/SetTimer.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class SetTimer extends AppCompatActivity {
     public static final String EXTRA_TIME = "com.example.simplecountdowntimer.TIME";
+    public static final String EXTRA_NAME = "com.example.simplecountdowntimer.NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +34,12 @@ public class SetTimer extends AppCompatActivity {
     public void loadTime (View view) {
         Log.i("Tag", "loadTime called");
 
+        TextView name1 = findViewById(R.id.editText);
         TextView minutes1 = findViewById(R.id.editText2);
         TextView seconds1 = findViewById(R.id.editText3);
         String minutes2 = minutes1.getText().toString();
         String seconds2 = seconds1.getText().toString();
+        String name2 = name1.getText().toString();
 
         long minutesInMilliseconds = 60000 * Integer.parseInt(minutes2);
         long secondsInMilliseconds = 1000 * Integer.parseInt(seconds2);
@@ -48,6 +51,7 @@ public class SetTimer extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra(EXTRA_TIME, totalTimeInMilliseconds);
+        intent.putExtra(EXTRA_NAME, name2);
 
         startActivity(intent);
     }
